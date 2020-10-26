68e3b47c6ddf72be8df8d0e34ea966205825b545
sam <sam@freighttrust.com>
2020-10-25 19:36:48 -0700

    v0.0.2
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 78,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: false,
+    trailingComma: "es5",
+    proseWrap: "never",
+    endOfLine: "lf",
+}
