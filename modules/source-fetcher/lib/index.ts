d5b429a410f8d1ddcf6244e9a50bb2af0e5fc82f
sam <sam@freighttrust.com>
2021-03-29 10:08:24 -0700

    feat(modules): init

---
+++ modules/source-fetcher/lib/index.ts
@@ -0,0 +1,10 @@
+import {Fetcher, FetcherConstructor} from "./types";
+import {InvalidNetworkError} from "./common";
+export {Fetcher, FetcherConstructor, InvalidNetworkError};
+
+import EtherscanFetcher from "./etherscan";
+import SourcifyFetcher from "./sourcify";
+
+const Fetchers: FetcherConstructor[] = [EtherscanFetcher, SourcifyFetcher];
+
+export default Fetchers;
